74665a98d9560c850ba5d63aa8879a8d385681d0
Shinnosuke Kondo <shinn.kondo@gmail.com>
2018-08-10 19:13:51 +0900

    Update some tests and their setting

--- tests/personal/suicaparser_test.py
+++ tests/personal/suicaparser_test.py
@@ -10,15 +10,20 @@ class TestSuicaParser():
 
     def setUp(self):
         self.parser = SuicaParser()
+        self.date = datetime.datetime(2018, 7, 30, 0, 00).date()
+    
 
     def test_is_applicable(self):
         ok_(self.parser.is_applicable("カードID,00000000000000"))
+        
+    def test_is_applicable_false(self):
+        assert not self.parser.is_applicable("00000000000000")
 
     def test_extract_fields_negative(self):
         row = ["2018/07/30", "自動販売機", "物品購入", "","","","","268","4881",""]
         eq_(self.parser.extract_fields(row),
             PartialTransactionInfo(
-                datetime.datetime(2018, 7, 30, 0, 00).date(),
+               self.date,               
                 "自動販売機, 物品購入",
                 -268)
         )
@@ -27,42 +32,42 @@ class TestSuicaParser():
         row = ["2018/07/30", "Charge", "", "","","","","","4881","3000"]
         eq_(self.parser.extract_fields(row),
             PartialTransactionInfo(
-                datetime.datetime(2018, 7, 30, 0, 00).date(),
+                self.date,
                 "Charge",
                 3000)
         )
     
     def test_choose_given_account_card(self):
         info =  PartialTransactionInfo(
-                datetime.datetime(2018, 7, 30, 0, 00).date(),
+                self.date,
                 "Charge",
                 3000)
         eq_(self.parser.choose_giving_account(info), MyAccount.CARD)
     
     def test_choose_given_account_trans(self):
         info =  PartialTransactionInfo(
-            datetime.datetime(2018, 7, 30, 0, 00).date(),
+            self.date,
             "自動改札機",
             -200)
         eq_(self.parser.choose_giving_account(info), MyAccount.TRANSPORTATION)
         
     def test_choose_given_account_unknown(self):
         info =  PartialTransactionInfo(
-            datetime.datetime(2018, 7, 30, 0, 00).date(),
+            self.date,
             "Dining",
             -2000)
         eq_(self.parser.choose_giving_account(info), MyAccount.UNKNOWN)
 
     def test_choose_given_account_meals(self):
         info =  PartialTransactionInfo(
-        datetime.datetime(2018, 7, 30, 0, 00).date(),
+        self.date,
         "Dining",
         -500)
         eq_(self.parser.choose_giving_account(info), MyAccount.MEALS)
 
     def test_choose_given_account_snack(self):
         info =  PartialTransactionInfo(
-        datetime.datetime(2018, 7, 30, 0, 00).date(),
+        self.date,
         "Dining",
         -100)
         eq_(self.parser.choose_giving_account(info), MyAccount.SNACK)
