060362a1bef1a73699e21d9ff70953de07d90652
Shinnosuke Kondo <shinn.kondo@gmail.com>
2018-08-10 18:54:06 +0900

    Tweak to use nose instead of unittest

--- tests/personal/suicaparser_test.py
+++ tests/personal/suicaparser_test.py
@@ -1,21 +1,22 @@
-from unittest import TestCase
+
+from nose.tools import ok_, eq_
 
 import datetime
 from gnucash_csv_importer.personal.suicaparser import SuicaParser
 from gnucash_csv_importer.personal.myaccount import MyAccount
 from gnucash_csv_importer.common import PartialTransactionInfo
 
-class TestSuicaParser(TestCase):
+class TestSuicaParser():
 
     def setUp(self):
         self.parser = SuicaParser()
 
     def test_is_applicable(self):
-        self.assertTrue(self.parser.is_applicable("カードID,00000000000000"))
+        ok_(self.parser.is_applicable("カードID,00000000000000"))
 
     def test_extract_fields_negative(self):
         row = ["2018/07/30", "自動販売機", "物品購入", "","","","","268","4881",""]
-        self.assertEqual(self.parser.extract_fields(row),
+        eq_(self.parser.extract_fields(row),
             PartialTransactionInfo(
                 datetime.datetime(2018, 7, 30, 0, 00).date(),
                 "自動販売機, 物品購入",
@@ -24,7 +25,7 @@ class TestSuicaParser(TestCase):
 
     def test_extract_fields_positive(self):
         row = ["2018/07/30", "Charge", "", "","","","","","4881","3000"]
-        self.assertEqual(self.parser.extract_fields(row),
+        eq_(self.parser.extract_fields(row),
             PartialTransactionInfo(
                 datetime.datetime(2018, 7, 30, 0, 00).date(),
                 "Charge",
@@ -36,32 +37,32 @@ class TestSuicaParser(TestCase):
                 datetime.datetime(2018, 7, 30, 0, 00).date(),
                 "Charge",
                 3000)
-        self.assertEqual(self.parser.choose_giving_account(info), MyAccount.CARD)
+        eq_(self.parser.choose_giving_account(info), MyAccount.CARD)
     
     def test_choose_given_account_trans(self):
         info =  PartialTransactionInfo(
             datetime.datetime(2018, 7, 30, 0, 00).date(),
             "自動改札機",
             -200)
-        self.assertEqual(self.parser.choose_giving_account(info), MyAccount.TRANSPORTATION)
+        eq_(self.parser.choose_giving_account(info), MyAccount.TRANSPORTATION)
         
     def test_choose_given_account_unknown(self):
         info =  PartialTransactionInfo(
             datetime.datetime(2018, 7, 30, 0, 00).date(),
             "Dining",
             -2000)
-        self.assertEqual(self.parser.choose_giving_account(info), MyAccount.UNKNOWN)
+        eq_(self.parser.choose_giving_account(info), MyAccount.UNKNOWN)
 
     def test_choose_given_account_meals(self):
         info =  PartialTransactionInfo(
         datetime.datetime(2018, 7, 30, 0, 00).date(),
         "Dining",
         -500)
-        self.assertEqual(self.parser.choose_giving_account(info), MyAccount.MEALS)
+        eq_(self.parser.choose_giving_account(info), MyAccount.MEALS)
 
     def test_choose_given_account_snack(self):
         info =  PartialTransactionInfo(
         datetime.datetime(2018, 7, 30, 0, 00).date(),
         "Dining",
         -100)
-        self.assertEqual(self.parser.choose_giving_account(info), MyAccount.SNACK)
+        eq_(self.parser.choose_giving_account(info), MyAccount.SNACK)
