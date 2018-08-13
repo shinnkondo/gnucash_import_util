f2c09cdf9b6b25d07da2833ee369b2aa2fa4a5c7
Shinnosuke Kondo <shinn.kondo@gmail.com>
2018-08-13 20:33:58 +0900

    Fix to check reconciled transactions

--- gnucash_csv_importer/reconciler.py
+++ gnucash_csv_importer/reconciler.py
@@ -8,8 +8,6 @@ class Reconciler:
     def __init__(self, split_candidates: List[Split]):
         self.split_candidates: Dict[datetime.date, Split] = {} 
         for split in split_candidates:
-            if split.reconcile_state == "y":
-                continue
 
             if not split.transaction.post_date in self.split_candidates:
                 self.split_candidates[split.transaction.post_date] = {split.value: [split]}
@@ -21,10 +19,7 @@ class Reconciler:
                 value2split[split.value] = [split]
                 continue
 
-            warnings.warn("There exists unreconciled split with the same date and the value in ", split.transaction.description, ". Reconciliation may be inaccurate.")
             value2split[split.value].append(split) 
-                
-            
 
     def reconcile(self, date, price):
         """Try to reconcile the splits and return the result. If success, mark the split.
@@ -32,8 +27,12 @@ class Reconciler:
         """
         
         if date in self.split_candidates and price in self.split_candidates[date]:
+            splits = self.split_candidates[date][price]
+            if len(splits) > 1 and splits[-2].reconcile_state != "y":
+                warnings.warn(f"There exists unreconciled split with the same date and the value in { splits[-1].transaction.description}. Reconciliation may be inaccurate.")
             # Only mark the last one as reconciled.
-            self.split_candidates[date][price][-1].reconcile_state = "c"
+            if splits[-1].reconcile_state != "y":
+                splits[-1].reconcile_state = "c"
             return True
         else:
             return False
